300e7e4ce3978146d718fab26e3907f7a5a31d8e
IT26102064 <it26102064@my.sliit.lk>
2026-06-24 13:10:24 +0530

    Add files via upload

--- Q3/IT26102064Lab2Q3.java
+++ Q3/IT26102064Lab2Q3.java
@@ -1,12 +1,10 @@
-public class IT26102191Lab2Q3
-{
-    public static void main(String[] args)
-    {
+public class IT26102064Lab2Q3 {
+    public static void main(String[] args) {
         double sideA = 3;
         double sideB = 4;
 
-        double hypotenuse = Math.sqrt((sideA * sideA) + (sideB * sideB));
+        double hypotenuse = Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2));
 
-        System.out.println("Length of the hypotenuse: " + hypotenuse);
+        System.out.println("Hypotenuse: " + hypotenuse);
     }
 }
